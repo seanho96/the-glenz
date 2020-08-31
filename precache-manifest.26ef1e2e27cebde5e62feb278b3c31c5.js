88105581d3b23672347e36487a92ef8726185b3c
Sean <seanho96@yahoo.com>
2020-08-31 15:09:08 +0800

    Updates

---
+++ precache-manifest.26ef1e2e27cebde5e62feb278b3c31c5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8c2049a15116010ae3efe855ddecd9e8",
+    "url": "/the-glenz/index.html"
+  },
+  {
+    "revision": "69cb603df86962da53ea",
+    "url": "/the-glenz/static/css/main.2905a80e.chunk.css"
+  },
+  {
+    "revision": "9349af1ce9ff0cab16ce",
+    "url": "/the-glenz/static/js/2.e66111d6.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/the-glenz/static/js/2.e66111d6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "69cb603df86962da53ea",
+    "url": "/the-glenz/static/js/main.4cbdfbd8.chunk.js"
+  },
+  {
+    "revision": "148f94ade0cffde9e57c",
+    "url": "/the-glenz/static/js/runtime-main.ea389aee.js"
+  }
+]);
